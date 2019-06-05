11bc132ed993ed62bbed460c01605f106c1e7b15
Shivangi Garg <33060783+shivangii-garg@users.noreply.github.com>
2019-06-05 22:49:17 +0530

    Update Explore.js

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -31,22 +31,22 @@ class Explore extends Component{
 
 	render() {
 		return (
-			<main className=" tc ">
-			
-
-					<nav className ='f3 pv3 ' style = {{display: 'flex',justifyContent: 'flex-end' , justifyContent: 'space-around'}}>
+			<main className=" tc  ">
+			<nav className ='f3 pv3 ' style = {{display: 'flex',justifyContent: 'flex-end' , justifyContent: 'space-around'}}>
 				<NavLink to ='/navigation' className='link dim black pointer '> Home </NavLink>
 			</nav>
 			<input type = "text" name='movieName' onChange = {this.handleChange} value = {this.state.movie_name}/>
 			<button className="b pb0 ph3 pv2 white bg-black grow pointer f6 dib" onClick = {this.getData}> Search </button>
+			<div className = "dib br3 pa3 ma2 grow bg-light-blue flex flex-column w5 tc">
 			<h1>{this.state.details.Title}</h1>
-			<h1>{this.state.details.Released}</h1>
-			<h1>{this.state.details.Genre}</h1>
-			<h1>{this.state.details.imdbRating}</h1>
-
+			<h2>{this.state.details.Released}</h2>
+			<h2>{this.state.details.Genre}</h2>
+			<h2>{this.state.details.imdbRating}</h2>
+			</div>
 			</main>
 			);
 	}
 }
 
 export default Explore;
+	
