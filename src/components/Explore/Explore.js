0c37ed359ce881c4bfb5ab4d71c57ce59a178e77
Shivangi Garg <33060783+shivangii-garg@users.noreply.github.com>
2019-06-05 21:25:21 +0530

    Update Explore.js

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -1,20 +1,52 @@
-import React from 'react';
-import Movie from '../Movie/Movie';
+import React,{Component} from 'react';
 import { NavLink } from 'react-router-dom';
+import axios from "axios";
 
-const Explore = () => {
-	return (
-		<main>
-		<nav className ='f3 pv3 ' style = {{display: 'flex',justifyContent: 'flex-end' , justifyContent: 'space-around'}}>
-			<NavLink to ='/' className='link dim black pointer '> Signout </NavLink>		
-			</nav>		
-		<form className = "tc pv5 pb4 f4" >
-			<input type = "text" name='movieName'/>
-			<button class="b pb0 ph3 pv2 white bg-black grow pointer f6 dib"> Search </button>
-			<Movie/>
-		</form>
-		</main>
-		);
+class Explore extends Component{
+	constructor(){
+		super();
+		this.state = {
+			details:{},
+			movie_name:''
+		}
+		this.getData = this.getData.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({movie_name: event.target.value});
+	}
+
+	getData () {
+		console.log("start");
+		axios.get(`http://www.omdbapi.com/?t=${this.state.movie_name}&apikey=168d291`)
+		.then(res => {
+			console.log(res.data);
+			this.setState({details:res.data});
+			console.log(this.state.details);
+		})
+		console.log("End");
+		console.log(this.state.details.Title);
+	}
+
+	render() {
+		return (
+			<main className=" tc ">
+			
+
+					<nav className ='f3 pv3 ' style = {{display: 'flex',justifyContent: 'flex-end' , justifyContent: 'space-around'}}>
+				<NavLink to ='/navigation' className='link dim black pointer '> Home </NavLink>
+			</nav>
+			<input type = "text" name='movieName' onChange = {this.handleChange} value = {this.state.movie_name}/>
+			<button className="b pb0 ph3 pv2 white bg-black grow pointer f6 dib" onClick = {this.getData}> Search </button>
+			<h1>{this.state.details.Title}</h1>
+			<h1>{this.state.details.Released}</h1>
+			<h1>{this.state.details.Genre}</h1>
+			<h1>{this.state.details.imdbRating}</h1>
+
+			</main>
+			);
+	}
 }
 
 export default Explore;
